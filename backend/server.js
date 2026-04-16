214d2c04130e8b7eed41bb1db47d5c41b813fad6
Govind Prajapati <prajapatigovind2210@gmail.com>
2026-04-16 15:36:02 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ app.use("/api/tasks", require("./routes/taskRoutes"));
 console.log("ENV:", process.env.MONGO_URI);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ DB Connected");
   })
