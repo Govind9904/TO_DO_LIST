41c8d78d94b2be49e1880ac8566a922c5d36923d
Govind Prajapati <prajapatigovind2210@gmail.com>
2026-04-16 15:39:04 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,14 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ DB Connected");
+
+    // START SERVER ONLY AFTER DB CONNECT
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("❌ DB Error:", err.message);
   });
-
-app.listen(8000, "0.0.0.0",() => console.log("Server running on port 6000"));
